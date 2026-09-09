280d1e6f956ae35a15fe331387f5853c0b0e5b56
anshusharma-dev <anshu.sharma@8848digital.com>
2026-09-09 06:32:42 +0000

    filter not loaded issue

--- rarc_customization/public/js/rarc_twd_filter.js
+++ rarc_customization/public/js/rarc_twd_filter.js
@@ -1,43 +1,32 @@
-$(document).on("page-change", function () {
-    setTimeout(function () {
-        if (
-            !frappe.query_report ||
-            frappe.query_report.report_name !== "Tax Withholding Details"
-        ) {
-            return;
-        }
+frappe.query_reports["Tax Withholding Details"] = frappe.query_reports["Tax Withholding Details"] || {};
 
-        if (frappe.query_report._profit_center_filter_added) {
-            return;
-        }
-
-        frappe.query_report.page.add_field({
+$.extend(frappe.query_reports["Tax Withholding Details"], {
+    onload: function (report) {
+        report.page.add_field({
             fieldname: "profit_center",
             label: __("Profit Center"),
             fieldtype: "Link",
             options: "Profit Center",
             change: function () {
-                var pc = frappe.query_report.get_filter_value("profit_center");
+                var pc = report.get_filter_value("profit_center");
 
                 if (pc) {
                     frappe.db.get_value("Profit Center", pc, "profit_center_name", function (r) {
-                        frappe.query_report.set_filter_value("profit_center_name", r.profit_center_name || "");
-                        frappe.query_report.refresh();
+                        report.set_filter_value("profit_center_name", r.profit_center_name || "");
+                        report.refresh();
                     });
                 } else {
-                    frappe.query_report.set_filter_value("profit_center_name", "");
-                    frappe.query_report.refresh();
+                    report.set_filter_value("profit_center_name", "");
+                    report.refresh();
                 }
             }
         });
 
-        frappe.query_report.page.add_field({
+        report.page.add_field({
             fieldname: "profit_center_name",
             label: __("Profit Center Name"),
             fieldtype: "Data",
             read_only: 1,
         });
-
-        frappe.query_report._profit_center_filter_added = true;
-    }, 500);
+    }
 });
