e54e7866be34d6e18e2ce5a01e18e1748561fefd
anshusharma-dev <anshu.sharma@8848digital.com>
2026-09-15 06:02:03 +0000

    accounting dimention

--- rarc_customization/public/js/purchase_invoice.js
+++ rarc_customization/public/js/purchase_invoice.js
@@ -16,6 +16,14 @@ frappe.ui.form.on('Purchase Invoice', {
         set_profit_center_in_child_tables(frm);
     },
 
+    cost_center: function(frm) {
+        set_cost_center_in_child_tables(frm);
+    },
+
+    business_place: function(frm) {
+        set_business_place_in_child_tables(frm);
+    },
+
     company: function(frm) {
         set_itc_ineligible_if_trust(frm);
     },
@@ -32,6 +40,16 @@ frappe.ui.form.on('Purchase Invoice Item', {
         // Profit center auto set
         if (frm.doc.profit_center) {
             frappe.model.set_value(cdt, cdn, 'profit_center', frm.doc.profit_center);
+        }
+
+        // Cost center auto set
+        if (frm.doc.cost_center) {
+            frappe.model.set_value(cdt, cdn, 'cost_center', frm.doc.cost_center);
+        }
+
+        // Business place auto set
+        if (frm.doc.business_place) {
+            frappe.model.set_value(cdt, cdn, 'business_place', frm.doc.business_place);
         }
 
         // Trust company hone par ITC ineligible auto set
@@ -53,6 +71,12 @@ frappe.ui.form.on('Purchase Taxes and Charges', {
         if (frm.doc.profit_center) {
             frappe.model.set_value(cdt, cdn, 'profit_center', frm.doc.profit_center);
         }
+        if (frm.doc.cost_center) {
+            frappe.model.set_value(cdt, cdn, 'cost_center', frm.doc.cost_center);
+        }
+        if (frm.doc.business_place) {
+            frappe.model.set_value(cdt, cdn, 'business_place', frm.doc.business_place);
+        }
     }
 });
 
@@ -68,6 +92,34 @@ function set_profit_center_in_child_tables(frm) {
 
     (frm.doc.taxes || []).forEach(function (row) {
         row.profit_center = frm.doc.profit_center;
+    });
+    frm.refresh_field('taxes');
+}
+
+function set_cost_center_in_child_tables(frm) {
+    if (!frm.doc.cost_center) return;
+
+    (frm.doc.items || []).forEach(function (row) {
+        row.cost_center = frm.doc.cost_center;
+    });
+    frm.refresh_field('items');
+
+    (frm.doc.taxes || []).forEach(function (row) {
+        row.cost_center = frm.doc.cost_center;
+    });
+    frm.refresh_field('taxes');
+}
+
+function set_business_place_in_child_tables(frm) {
+    if (!frm.doc.business_place) return;
+
+    (frm.doc.items || []).forEach(function (row) {
+        row.business_place = frm.doc.business_place;
+    });
+    frm.refresh_field('items');
+
+    (frm.doc.taxes || []).forEach(function (row) {
+        row.business_place = frm.doc.business_place;
     });
     frm.refresh_field('taxes');
 }
--- rarc_customization/public/js/sales_invoice.js
+++ rarc_customization/public/js/sales_invoice.js
@@ -1,6 +1,18 @@
 frappe.ui.form.on('Sales Invoice', {
     profit_center: function(frm) {
         set_profit_center_in_child_tables(frm);
+    },
+
+    cost_center: function(frm) {
+        set_cost_center_in_child_tables(frm);
+    },
+
+    business_place: function(frm) {
+        set_business_place_in_child_tables(frm);
+    },
+
+    project: function(frm) {
+        set_project_in_child_tables(frm);
     }
 });
 
@@ -9,6 +21,15 @@ frappe.ui.form.on('Sales Invoice Item', {
         if (frm.doc.profit_center) {
             frappe.model.set_value(cdt, cdn, 'profit_center', frm.doc.profit_center);
         }
+        if (frm.doc.cost_center) {
+            frappe.model.set_value(cdt, cdn, 'cost_center', frm.doc.cost_center);
+        }
+        if (frm.doc.business_place) {
+            frappe.model.set_value(cdt, cdn, 'business_place', frm.doc.business_place);
+        }
+        if (frm.doc.project) {
+            frappe.model.set_value(cdt, cdn, 'project', frm.doc.project);
+        }
     }
 });
 
@@ -16,6 +37,15 @@ frappe.ui.form.on('Sales Taxes and Charges', {
     taxes_add: function(frm, cdt, cdn) {
         if (frm.doc.profit_center) {
             frappe.model.set_value(cdt, cdn, 'profit_center', frm.doc.profit_center);
+        }
+        if (frm.doc.cost_center) {
+            frappe.model.set_value(cdt, cdn, 'cost_center', frm.doc.cost_center);
+        }
+        if (frm.doc.business_place) {
+            frappe.model.set_value(cdt, cdn, 'business_place', frm.doc.business_place);
+        }
+        if (frm.doc.project) {
+            frappe.model.set_value(cdt, cdn, 'project', frm.doc.project);
         }
     }
 });
@@ -30,6 +60,48 @@ function set_profit_center_in_child_tables(frm) {
 
     (frm.doc.taxes || []).forEach(function (row) {
         row.profit_center = frm.doc.profit_center;
+    });
+    frm.refresh_field('taxes');
+}
+
+function set_cost_center_in_child_tables(frm) {
+    if (!frm.doc.cost_center) return;
+
+    (frm.doc.items || []).forEach(function (row) {
+        row.cost_center = frm.doc.cost_center;
+    });
+    frm.refresh_field('items');
+
+    (frm.doc.taxes || []).forEach(function (row) {
+        row.cost_center = frm.doc.cost_center;
+    });
+    frm.refresh_field('taxes');
+}
+
+function set_business_place_in_child_tables(frm) {
+    if (!frm.doc.business_place) return;
+
+    (frm.doc.items || []).forEach(function (row) {
+        row.business_place = frm.doc.business_place;
+    });
+    frm.refresh_field('items');
+
+    (frm.doc.taxes || []).forEach(function (row) {
+        row.business_place = frm.doc.business_place;
+    });
+    frm.refresh_field('taxes');
+}
+
+function set_project_in_child_tables(frm) {
+    if (!frm.doc.project) return;
+
+    (frm.doc.items || []).forEach(function (row) {
+        row.project = frm.doc.project;
+    });
+    frm.refresh_field('items');
+
+    (frm.doc.taxes || []).forEach(function (row) {
+        row.project = frm.doc.project;
     });
     frm.refresh_field('taxes');
 }
